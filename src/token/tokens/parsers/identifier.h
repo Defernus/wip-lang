0f77129b2b22556c4d3c30be6ffe593893888a92
Defernus <dehucka98@gmail.com>
2021-12-30 00:03:29 +0300

    add underscore to identifier token

--- src/token/tokens/parsers/identifier.h
+++ src/token/tokens/parsers/identifier.h
@@ -1,11 +1,15 @@
 
 ChopToken(identifier, token_start, error) {
-  if (!stringContainsChar(UPPER_CASE_LETTERS, *token_start) && !stringContainsChar(LOWER_CASE_LETTERS, *token_start)) {
+  if (
+    !stringContainsChar(UPPER_CASE_LETTERS, *token_start) &&
+    !stringContainsChar(LOWER_CASE_LETTERS, *token_start) &&
+    *token_start != '_'
+  ) {
     return NULL;
   }
 
   char *token_end = token_start + 1;
-  while (checkIfLetterOrDigit(*token_end)) ++token_end;
+  while (*token_end == '_' || checkIfLetterOrDigit(*token_end)) ++token_end;
 
   return token_end;
 }
